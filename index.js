61936fda1f97c84deff97ab0646bf234d9287d46
ice-rahul <meet4g@gmail.com>
2021-02-20 22:22:39 +0530

    Added online course

--- index.js
+++ index.js
@@ -25,3 +25,45 @@ const goToContact = () => {
     behavior: 'smooth',
   });
 };
+
+const goToWomenEmpowerment = () => {
+  window.scrollTo({
+    top: document.getElementById('women-empowerment').offsetTop,
+    behavior: 'smooth',
+  });
+};
+
+const goToOnlineFitness = () => {
+  window.scrollTo({
+    top: document.getElementById('online-fitness').offsetTop,
+    behavior: 'smooth',
+  });
+};
+const showPricing = (selectedElement, selectedPricing) => {
+  document.querySelectorAll('.pricingTable').forEach((val) => {
+    val.classList.add('hidden');
+  });
+  document.querySelectorAll('.tabs').forEach((val) => {
+    val.classList.remove('bg-green-500');
+    val.classList.remove('text-white');
+  });
+  document.getElementById(selectedPricing).classList.remove('hidden');
+  document.getElementById(selectedElement).classList.add('bg-green-500');
+  document.getElementById(selectedElement).classList.add('text-white');
+};
+
+const goToOnlinePricing = () => {
+  showPricing('online', 'onlinePricing');
+  window.scrollTo({
+    top: document.getElementById('online-pricing').offsetTop,
+    behavior: 'smooth',
+  });
+};
+
+const goToWomenEmpowermentPricing = () => {
+  showPricing('offline', 'offlinePricing');
+  window.scrollTo({
+    top: document.getElementById('online-pricing').offsetTop,
+    behavior: 'smooth',
+  });
+};
